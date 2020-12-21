0a6495cf24f9e94005c12fd8f67f161326a8cc26
denissden <denissden1337@gmail.com>
2020-12-21 15:54:42 +0300

    now the result is the .graphlm file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Node import Node
 from Way import Way
 from BoundingBox import BoundingBox
 import matplotlib.pyplot as plt
-from functions import merge_ways
+from functions import merge_ways, translit1
 from pprint import pprint
 
 
@@ -33,24 +33,24 @@ lat2 = 55.755245
 long2 = 37.625251
 
 # JUST WHY
-# lat1 = 55.709117
-# long1 = 37.601357
-#
-# lat2 = 55.805245
-# long2 = 37.705251
+lat1 = 55.709117
+long1 = 37.601357
+
+lat2 = 55.805245
+long2 = 37.705251
 
 # BRO YOU ARE INSANE
-# lat1 = 55.609117
-# long1 = 37.501357
+# lat1 = 55.559117
+# long1 = 37.451357
 #
-# lat2 = 55.905245
-# long2 = 37.805251
+# lat2 = 55.955245
+# long2 = 37.855251
 
 bbox = BoundingBox(lat1, long1, lat2, long2)
 query = f"""
 [out:json]
 [bbox:{bbox}];
-way[highway~"^(service|primary|secondary)$"];
+way[highway~"^(primary|secondary|service)$"];
 (._;>;);
 out center; 
 """
@@ -84,9 +84,10 @@ print(*nodes.values(), sep='\n')
 #print(*ways, sep='\n')
 
 way_dict = merge_ways(nodes, ways)
-pprint(way_dict.dictionary)
+# pprint(way_dict.dictionary)
+pprint(nodes)
 
-
+'''
 plt.xlabel("x")
 plt.ylabel("y")
 # plt.figure(num=None, figsize=(60, 40), dpi=70)
@@ -132,5 +133,35 @@ for way in ways:
     plt.plot(_x, _y, )
 
 plt.show()
+'''
+
+end_file = ""
+c = 0
+id_dict = dict()
+for k in way_dict.dictionary.keys():
+    id_dict[k] = c
+    node = nodes[k]
+    end_file += f'<node positionX="{int(node.x)}" positionY="{int(node.y)}" id="{c}" mainText="Graph_{c}" upText="" ></node>'
+    c += 1
+
+end_file = '<?xml version="1.0" encoding="UTF-8"?>' \
+           '<graphml>' \
+           '<graph id="Graph" uidGraph="4" uidEdge="10015">' + end_file
+print(c)
+for way in ways:
+    c += 1
+    id1 = id_dict[way.end1]
+    id2 = id_dict[way.end2]
+    end_file += f'<edge source="{id1}" target="{id2}" ' \
+                f'isDirect="true" weight="1"' \
+                f' useWeight="true" id="{c}"' \
+                f' text="{translit1(way.name)}" upText="{translit1(way.name)}" arrayStyleStart="" ' \
+                f'arrayStyleFinish="" model_width="4" model_type="0"' \
+                f' model_curvedValue="0.1" ></edge>'
+
+print(c)
+end_file += '</graph></graphml>'
 
 
+with open("test2.graphml", "w+") as f:
+    f.write(end_file)
